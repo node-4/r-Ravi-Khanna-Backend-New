c52cf5414d2448f2183f2ff9d7b68a7740f346ae
rubiyadav <rubiflyeis@gmail.com>
2023-05-30 14:29:49 +0530

    update

--- routes/predict.route.js
+++ routes/predict.route.js
@@ -4,7 +4,7 @@ const { authJwt, objectId } = require("../middlewares");
 module.exports = (app) => {
     app.post("/api/v1/admin/predicts", [authJwt.isAdmin], predicts.createPrediction);
     app.put("/api/v1/admin/predicts/:id", [authJwt.isAdmin, objectId.validId], predicts.updatePredictionById);
-    app.get("/api/v1/admin/predicts", predicts.getAllPredictions);
+    app.post("/api/v1/admin/allPredicts", predicts.getAllPredictions);
     app.get("/api/v1/admin/predicts/:id", [objectId.validId], predicts.getPredictionById);
     app.delete("/api/v1/admin/predicts/:id", [authJwt.isAdmin, objectId.validId], predicts.deletePredictionById);
 
